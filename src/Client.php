74ad842a613357ed6c81df95bdf55d96f36c7464
SDK Builder <sdk-builder@alipay.com>
2026-09-23 14:02:44 +0800

    Generated SDK for ATO.

--- src/Client.php
+++ src/Client.php
@@ -855,7 +855,7 @@ class Client {
                     "req_msg_id" => UtilClient::getNonce(),
                     "access_key" => $this->_accessKeyId,
                     "base_sdk_version" => "TeaSDK-2.0",
-                    "sdk_version" => "1.20.20",
+                    "sdk_version" => "1.20.21",
                     "_prod_code" => "ATO",
                     "_prod_channel" => "undefined"
                 ];
